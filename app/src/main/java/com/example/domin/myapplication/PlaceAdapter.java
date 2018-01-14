d65c8799b80c3397c89fdc078dc731a654896ee5
lolstozalosiem <dominika.gabor@onet.eu>
2018-01-14 12:17:18 +0100

    gotowe

---
+++ app/src/main/java/com/example/domin/myapplication/PlaceAdapter.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by domin on 14.01.2018.
+ */
+
+public class PlaceAdapter {
+}
---
+++ app/src/main/java/com/example/domin/myapplication/SecActivity.java
@@ -0,0 +1,8 @@
+package com.example.domin.myapplication;
+
+/**
+ * Created by domin on 14.01.2018.
+ */
+
+public class SecActivity {
+}
